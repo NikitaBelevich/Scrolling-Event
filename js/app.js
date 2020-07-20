0055d659ca7c3cf44a20c52994a937778c47a143
Nikita Belevich <belevish_1998@mail.ru>
2020-07-20 15:41:31 +0300

    Task 1 complete

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
 'use strict';
 
+// Task 1
+
+const heightDocument = document.documentElement.scrollHeight;
+const lowerLimit = heightDocument - 100;
+
+// console.warn(currentHeight);
+
+window.addEventListener('scroll', (e) => {
+    const currentDate = new Date();
+    const currentHeight = document.documentElement.clientHeight;
+    const bottomDocument = document.documentElement.getBoundingClientRect().bottom;
+
+    if (bottomDocument < currentHeight + 100) {
+        document.body.insertAdjacentHTML('beforeend', `<p>Date: ${currentDate}</p>`);
+    }
+    console.log(bottomDocument, currentHeight);
+});
